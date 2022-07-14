e5c32ac8cc998809dbe8ca335d75809a2abba778
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-07-14 17:44:49 +0300

    add api-js

---
+++ src/js/api.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const KEY = '119d79b053f922516c6af4b71b0fd3ac';
+
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+
+export const IMG_BASE_URL = `https://image.tmdb.org/t/p`;
+export const IMG_W500 = `/w500`;
+export const IMG_W400 = `/w400`;
+export const IMG_W300 = `/w300`;
+export const IMG_W200 = `/w200`;
+export const IMG_W100 = `/w100`;
+export const IMG_ORIG = `/original`;
+
+export const getTrending = async (page = 1) => {
+  const { data } = await axios.get(
+    `/trending/movie/week?api_key=${KEY}&page=${page}`
+  );
+  return data;
+};
+
+// console.log(getTrending(2).then(r => console.log(r)));
+
+export const getSearchMovie = async (query, page) => {
+  const { data } = await axios.get(
+    `/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
+  );
+  return data;
+};
+
+// console.log(getSearchMovie('cat', 1).then(r => console.log(r)));
+
+export const getMovieDetails = async id => {
+  const { data } = await axios.get(`/movie/${id}?api_key=${KEY}`);
+  return data;
+};
+
+// console.log(getMovieDetails(5050).then(r => console.log(r)));
+
+export const getMovieGenres = async () => {
+  const { data } = await axios.get(`/genre/movie/list?api_key=${KEY}`);
+  return data;
+};
+
+// console.log(getMovieGenres().then(r => console.log(r)));
