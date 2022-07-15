7a5b4e8c5e634150820ea9481b79be7c5b600e1d
TkachAn <4ndrey.tkachenko@gmail.com>
2022-07-15 20:26:24 +0300

    Create buttons.js

---
+++ src/js/buttons.js
@@ -0,0 +1,4 @@
+const btnWatched = document.querySelector('.btn--watched');
+const btnQueue = document.querySelector('.btn--queue');
+const btnAddWatched = document.querySelector('.btn--addwatched');
+const btnAddQueue = document.querySelector('.btn--addqueue');
