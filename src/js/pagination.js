4abe9221b0d7252b2abba585dbaa1523d7c7a895
zhekichaan <eugene.chernitskiy@gmail.com>
2022-07-17 07:48:48 -0400

    pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,7 +5,11 @@ const paginationBar = document.querySelector('.pagination-btns')
 paginationBar.children[0].firstChild.classList.add('is-hidden')
 
 let page = 1
-const amountOfPages = 10
+const amountOfPages = 25
+
+if(amountOfPages==1) {
+	paginationBar.innerHTML= `<li class="page active"><span>1</li>`
+}
 
 paginationBar.children[9].textContent = amountOfPages
 
@@ -18,10 +22,10 @@ function onNextBtnClick() {
 		paginationBar.children[page+2].classList.add('active')
 		paginationBar.children[page+1].classList.remove('active')
 		paginationBar.children[0].firstChild.classList.remove('is-hidden')
-	} 
-	else if(page>=3) {
-		if(page<amountOfPages-2) {
-			page+=1
+	} else 
+	if(page>=3) {
+		page+=1
+		if(page<=amountOfPages-2) {
 			paginationBar.children[1].classList.remove('is-hidden')
 			paginationBar.children[2].classList.remove('is-hidden')
 			paginationBar.children[3].textContent = page - 2
@@ -29,28 +33,28 @@ function onNextBtnClick() {
 			paginationBar.children[5].textContent = page 
 			paginationBar.children[6].textContent = page + 1
 			paginationBar.children[7].textContent = page + 2
-		} else
-		if(page>amountOfPages-3) {
+		} 
+		if(page>=amountOfPages-2) {
 			paginationBar.children[8].classList.add('is-hidden')
 			paginationBar.children[9].classList.add('is-hidden')
-			if(page==amountOfPages-1) {
+		}
+		if(page==amountOfPages-1) {
 				paginationBar.children[5].classList.remove('active')
 				paginationBar.children[6].classList.add('active')
 			}
-			page+=1
 		}
 		if(page==amountOfPages) {
 			paginationBar.children[6].classList.remove('active')
 			paginationBar.children[7].classList.add('active')
+			paginationBar.children[10].classList.add('is-hidden')
 		}
-	} 
-
-
 		console.dir(paginationBar)
 		console.log(page)
 	}
 
-
+if(8>=8) {
+	console.log(1)
+}
 
 function onPrevBtnClick() {
 	if(page<4) {
@@ -62,7 +66,7 @@ function onPrevBtnClick() {
 		if(page==1) {
 			paginationBar.children[0].firstChild.classList.add('is-hidden')
 		}
-	} else if(page>=3) {
+	} else if(page>=3 && page<amountOfPages-2) {
 		page-=1
 
 		paginationBar.children[3].textContent = page - 2
@@ -76,5 +80,25 @@ function onPrevBtnClick() {
 			paginationBar.children[2].classList.add('is-hidden')
 		}
 	}
+	if(page==amountOfPages-2) {
+		page-=1
+		paginationBar.children[8].classList.remove('is-hidden')
+		paginationBar.children[9].classList.remove('is-hidden')
+		paginationBar.children[3].textContent = page - 2
+		paginationBar.children[4].textContent = page - 1
+		paginationBar.children[5].textContent = page 
+		paginationBar.children[6].textContent = page + 1
+		paginationBar.children[7].textContent = page + 2
+	} else if(page==amountOfPages-1) {
+		page-=1
+		paginationBar.children[5].classList.add('active')
+		paginationBar.children[6].classList.remove('active')
+	} else if(page==amountOfPages) {
+		page-=1
+		paginationBar.children[6].classList.add('active')
+		paginationBar.children[7].classList.remove('active')
+		paginationBar.children[10].classList.remove('is-hidden')
+	}
+
 	console.log(page)
 }
