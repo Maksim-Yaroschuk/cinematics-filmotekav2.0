d625d19c4146f8a70dba83ba155af583955a4720
TkachAn <4ndrey.tkachenko@gmail.com>
2022-07-18 18:01:02 +0300

    Update storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -54,8 +54,94 @@ btnWatched.addEventListener("click", filterWatched);
 btnQueue.addEventListener("click", filterQueue);
 btnAddWatched.addEventListener("click",funAddWatched );
 btnAddQueue.addEventListener("click", funAddQueue);
-poster.addEventListener("click", funAddQueue);
+//poster.addEventListener("click", funAddQueue);
 
+
+function funAddWatched() {
+	const WatchedArr = load(Watched);
+	console.log('WatchedArr', WatchedArr);
+	const id = Number(selectedMovie.getAttribute('key'));
+	console.log('id', id);
+	const index = WatchedArr.indexOf(id);
+	console.log('index',index);
+	if (index<0) {
+		WatchedArr.push(id);
+	} else {
+		WatchedArr.splice(id, 1);
+		console.log('WatchedArr', WatchedArr);
+	}
+	save('Watched', WatchedArr);
+	console.log('load', load(Watched));
+}
+
+function funAddQueue() {
+	const QueueArr = load(Queue);
+	console.log('QueueArr', QueueArr);
+	const id = Number(selectedMovie.getAttribute('key'));
+	console.log('id', id);
+	const index = QueueArr.indexOf(id);
+	console.log('index',index);
+	if (index<0) {
+		QueueArr.push(id);
+	} else {
+		QueueArr.splice(id, 1);
+		console.log('QueueArr', QueueArr);
+	}
+	save('Queue', QueueArr);
+	console.log('load', load(Queue));
+}
+
+
+const filterWatched = () => {
+	//const WatchedList = load(Watched);
+	console.log('WatchedArr', WatchedList);
+
+	// if (!WatchedList.length) {
+	// 	return console.log('ваш список пуст!');
+	// }
+	//  watchedList.map(() => {
+	// 	getMovieDetails();
+	// 	createListMarkup(data)
+	//  })
+	// renderMarkup(data);
+};
+
+
+const filterQueue = () => {
+	const QueueList = load(Queue);
+	console.log('QueueArr', QueueList);
+
+	if (!QueueList.length) {
+		return console.log('ваш список пуст!');
+	}
+	//  watchedList.map(() => {
+	// 	getMovieDetails();
+	// 	createListMarkup(data)
+	//  })
+	// renderMarkup(data);
+};
+
+const filterLiberty = (val) => {
+	const list = load(val);
+	console.log('WatchedArr', list);
+
+	if (!list.length) {
+		return console.log('ваш список пуст!');
+	}
+	 list.map(() => {
+		getSearchMovieId();
+		//getMovieDetails();
+		createListMarkup(data);
+	 })
+	renderMarkup(data);
+};
+
+const getSearchMovieId = async (id) => {
+  const { data } = await axios.get(
+    `/search/movie?api_key=${KEY}&language=en-US&id=${id}`
+  );
+  return data;
+};
 
 function funAddLib(val) {
 	const array = load(val);
@@ -73,90 +159,4 @@ function funAddLib(val) {
 	save(val, array);
 	console.log('load', load(val));
 }
-
-function funAddWatched() {
-	const WatchedArr = load(Watched);
-	console.log('WatchedArr', WatchedArr);
-	const id = Number(selectedMovie.getAttribute('key'));
-	console.log('id', id);
-	const index = WatchedArr.indexOf(id);
-	console.log('index',index);
-	if (index<0) {
-		WatchedArr.push(id);
-	} else {
-		WatchedArr.splice(id, 1);
-		console.log('WatchedArr', WatchedArr);
-	}
-	save('Watched', WatchedArr);
-	console.log('load(Watched)', load(Watched));
-}
-
-function funAddQueue() {
-	const QueueArr = load(Queue);
-	console.log('WatchedArr', QueueArr);
-	const id = Number(selectedMovie.getAttribute('key'));
-	console.log('id', id);
-	const index = QueueArr.indexOf(id);
-	console.log('index',index);
-	if (index<0) {
-		QueueArr.push(id);
-	} else {
-		QueueArr.splice(id, 1);
-		console.log('WatchedArr', QueueArr);
-	}
-	save('Watched', QueueArr);
-	console.log('load(Watched)', load(Queue));
-}
-
-
-const filterWatched = () => {
-	const WatchedList = load(Watched);
-	console.log('WatchedArr', WatchedArr);
-
-	if (!WatchedList.length) {
-		return console.log('ваш список пуст!');
-	}
-	 watchedList.map(() => {
-		getMovieDetails();
-		createListMarkup(data)
-	 })
-	renderMarkup(data);
-};
-
-
-const filterQueue = () => {
-	const WatchedList = load(Queue);
-	console.log('QueueArr', QueueList);
-
-	if (!WatchedList.length) {
-		return console.log('ваш список пуст!');
-	}
-	 watchedList.map(() => {
-		getMovieDetails();
-		createListMarkup(data)
-	 })
-	renderMarkup(data);
-};
-
-const filterLiberty = (val) => {
-	const List = load(val);
-	console.log('WatchedArr', List);
-
-	if (!List.length) {
-		return console.log('ваш список пуст!');
-	}
-	 watchedList.map(() => {
-		getSearchMovieId();
-		//getMovieDetails();
-		createListMarkup(data);
-	 })
-	renderMarkup(data);
-};
-
-const getSearchMovieId = async (id) => {
-  const { data } = await axios.get(
-    `/search/movie?api_key=${KEY}&language=en-US&id=${id}`
-  );
-  return data;
-};
 
