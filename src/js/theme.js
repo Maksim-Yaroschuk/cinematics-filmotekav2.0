cc769781c7970536f7543d23da255e544ba37903
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-07-20 22:35:46 +0300

    add to change icon

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,17 +3,21 @@ const toggleThemeImage = document.getElementById('toggle-theme-image');
 
 function setLightTheme() {
    document.body.classList.remove('dark');
-   toggleThemeImage.src = '../images/moon-line.svg';
+   toggleThemeImage.href.baseVal = '/icons.adfc4680.svg#icon-moon-line'
+   // toggleThemeImage.src = '../images/moon-line.svg';
    localStorage.theme = 'light';
 };
 
 function setDarkTheme() {
    document.body.classList.add('dark')
-   toggleThemeImage.src = '../images/day-sunny.svg';
+   toggleThemeImage.href.baseVal = '/icons.adfc4680.svg#icon-day-sunny'
+   // toggleThemeImage.src = '../images/day-sunny.svg';
    localStorage.theme = 'dark';
 };
 
 toggleThemeBtn.addEventListener('click', () => {
+   console.log(toggleThemeImage.href)
+   
    if (document.body.classList.contains('dark')) {
       setLightTheme(); 
    } else {
