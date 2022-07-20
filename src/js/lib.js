6432193b4934e7dc1f9f617472ca93b75b01fbad
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-07-20 19:46:48 +0300

    fix lib

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,32 +1,31 @@
 import { loadLs } from './storage';
-import { createListMarkup, createLibMarkup } from './renderMarkup';
+import { createListMarkup, renderLibMarkup } from './renderMarkup';
 import { lib } from './refs';
 
 const btnWatched = document.querySelector('.btn--watched');
 const btnQueue = document.querySelector('.btn--queue');
 
-if (btnWatched!==null) {
-	btnWatched.addEventListener("click", () => {
-		libMarkup('Watched');
-	});
-	btnQueue.addEventListener("click", () => {
-		libMarkup('Queue');
-	});
+if (btnWatched !== null) {
+  btnWatched.addEventListener('click', () => {
+    libMarkup('Watched');
+  });
+  btnQueue.addEventListener('click', () => {
+    libMarkup('Queue');
+  });
 }
 
-function libMarkup(selectLs) {
-	let sel = selectLs + 'Data';
-	console.log('sel', sel);
-	const QueueList = loadLs(selectLs);
-	console.log('List', QueueList);
-	if (!QueueList || !QueueList.length ) {
-		return console.log('ваш список пуст!');
-	}
-	let dat = loadLs(sel);
-	console.log('dat', dat);
-	// const markupList = createListMarkup(dat);
-	// console.log('markupList', markupList);
-	
-    lib.innerHTML = createLibMarkup(dat);
-};
-
+export function libMarkup(selectLs) {
+  let sel = selectLs + 'Data';
+  // console.log('sel', sel);
+  const QueueList = loadLs(selectLs);
+  // console.log('List', QueueList);
+  if (!QueueList || !QueueList.length) {
+    return console.log('ваш список пуст!');
+  }
+  let data = { results: loadLs(sel) };
+  // console.log('dat', data);
+  // const markupList = createListMarkup(data);
+  // console.log('markupList', markupList);
+  renderLibMarkup(data);
+  // console.log(data);
+}
--- src/js/storage.js
+++ src/js/storage.js
@@ -25,33 +25,33 @@ const removeLs = key => {
 };
 ///localStorage.clear();
 const moviesData = loadLs('moviesData');
-console.log('moviesData', moviesData);
+// console.log('moviesData', moviesData);
 const WatchedData = loadLs('WatchedData');
-console.log('WatchedData', WatchedData);
+// console.log('WatchedData', WatchedData);
 const QueueData = loadLs('QueueData');
-console.log('QueueData', QueueData);
+// console.log('QueueData', QueueData);
 const Watched = loadLs('Watched');
-console.log('Watched', Watched);
+// console.log('Watched', Watched);
 const Queue = loadLs('Queue');
-console.log('Queue', Queue);
+// console.log('Queue', Queue);
 
 function addListLibrary(id, select) {
-	const sel = select + 'Data';
-	const moviesData = loadLs('moviesData');
-	const movieData = moviesData.find(movie => movie.id === id);
-	const libArr = loadLs(select) ? loadLs(select) : [];
-	const libData = loadLs(sel) ? loadLs(sel) : [];
-	const index = libArr.indexOf(id);
-	if (index < 0) {
-		libArr.push(id);
-		libData.push(movieData);
-	} else {
-		libArr.splice(index, 1);
-		//const libData = loadLs(sel);
-		libData.splice(index, 1);
-	}
-	saveLs(select, libArr);
-	saveLs(sel, libData);
+  const sel = select + 'Data';
+  const moviesData = loadLs('moviesData');
+  const movieData = moviesData.find(movie => movie.id === id);
+  const libArr = loadLs(select) ? loadLs(select) : [];
+  const libData = loadLs(sel) ? loadLs(sel) : [];
+  const index = libArr.indexOf(id);
+  if (index < 0) {
+    libArr.push(id);
+    libData.push(movieData);
+  } else {
+    libArr.splice(index, 1);
+    //const libData = loadLs(sel);
+    libData.splice(index, 1);
+  }
+  saveLs(select, libArr);
+  saveLs(sel, libData);
 }
 
 // function funAddQueue(id) {
