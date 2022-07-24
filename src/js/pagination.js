caf4c900133ce96d39f2300a808afe2334d344fe
zhekichaan <eugene.chernitskiy@gmail.com>
2022-07-24 12:41:25 -0400

    pg fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -11,6 +11,7 @@ if (form) {
 const prevBtn = document.querySelector('.page-btn.prev');
 const nextBtn = document.querySelector('.page-btn.next');
 const paginationSection = document.querySelector('.pagination-section');
+const toMainBtn = document.querySelector('.to_main__link');
 
 const refs = {
 	filterForm: document.querySelector('#filter-form'),
@@ -19,6 +20,13 @@ const refs = {
   yearForm: document.querySelector('#yearForm'),
   btnReset: document.querySelector('#btnResetFilter'),
 };
+
+if (toMainBtn) {
+	toMainBtn.addEventListener('click', (e) => {
+		page = 1
+		saveLs('page-pg', page)
+	} )
+}
 
 if (refs.genreForm) {
   refs.genreForm.addEventListener('input', eventGenre);
