31da02bac1e8a17b10388ce239240ce4972306da
TkachAn <4ndrey.tkachenko@gmail.com>
2022-07-25 21:06:59 +0300

    Update storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,3 +1,4 @@
+import {getMovieDetails} from './api';
 const saveLs = (key, value) => {
   try {
     const serializedState = JSON.stringify(value);
@@ -24,28 +25,23 @@ const removeLs = key => {
   }
 };
 
-///localStorage.clear();
-const moviesData = loadLs('moviesData');
-// console.log('moviesData', moviesData);
-const WatchedData = loadLs('WatchedData');
-// console.log('WatchedData', WatchedData);
-const QueueData = loadLs('QueueData');
-// console.log('QueueData', QueueData);
-const Watched = loadLs('Watched');
-// console.log('Watched', Watched);
-const Queue = loadLs('Queue');
-// console.log('Queue', Queue);
 
-function addListLibrary(id, select) {
-  const sel = select + 'Data';
-  const moviesData = loadLs('moviesData');
-  const movieData = moviesData.find(movie => movie.id === id);
-  const libArr = loadLs(select) ? loadLs(select) : [];
-  const libData = loadLs(sel) ? loadLs(sel) : [];
-  const index = libArr.indexOf(id);
+async function addListLibrary(id, select) {
+	const movData = await getMovieDetails(id);
+//console.log('movData',movData);
+	const sel = select + 'Data';
+	// const moviesData = loadLs('moviesData');
+	// const movieData = moviesData.find(movie => movie.id === id);
+	//const movieData = await getMovieDetails(id);
+	// console.log('moviesData', moviesData);
+	// console.log('movieData', movieData);
+	console.log('movData',movData);
+	const libArr = loadLs(select) ? loadLs(select) : [];
+	const libData = loadLs(sel) ? loadLs(sel) : [];
+ 	const index = libArr.indexOf(id);
   if (index < 0) {
     libArr.push(id);
-    libData.push(movieData);
+    libData.push(movData);
   } else {
     libArr.splice(index, 1);
     //const libData = loadLs(sel);
@@ -56,7 +52,41 @@ function addListLibrary(id, select) {
 
 }
 
+
+
+// ///localStorage.clear();
+// const moviesData = loadLs('moviesData');
+// // console.log('moviesData', moviesData);
+// const WatchedData = loadLs('WatchedData');
+// // console.log('WatchedData', WatchedData);
+// const QueueData = loadLs('QueueData');
+// // console.log('QueueData', QueueData);
+// const Watched = loadLs('Watched');
+// // console.log('Watched', Watched);
+// const Queue = loadLs('Queue');
+// // console.log('Queue', Queue);
+
+// function addListLibrary(id, select) {
+//   const sel = select + 'Data';
+//   const moviesData = loadLs('moviesData');
+//   const movieData = moviesData.find(movie => movie.id === id);
+//   const libArr = loadLs(select) ? loadLs(select) : [];
+//   const libData = loadLs(sel) ? loadLs(sel) : [];
+//   const index = libArr.indexOf(id);
+//   if (index < 0) {
+//     libArr.push(id);
+//     libData.push(movieData);
+//   } else {
+//     libArr.splice(index, 1);
+//     //const libData = loadLs(sel);
+//     libData.splice(index, 1);
+//   }
+//   saveLs(select, libArr);
+//   saveLs(sel, libData);
+
+// }
+
 export { addListLibrary, funAddQueue, saveLs, loadLs, removeLs };
 export function moviesDataUpdate(data) {
-  localStorage.setItem('moviesData', JSON.stringify(data.results));
+localStorage.setItem('moviesData', JSON.stringify(data.results));
 }
