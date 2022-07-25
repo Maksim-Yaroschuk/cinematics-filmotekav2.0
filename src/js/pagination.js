f921fd16e79854f70dcd9d799e3c2c83d93fb001
zhekichaan <eugene.chernitskiy@gmail.com>
2022-07-25 19:35:59 -0400

    new spinner

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,6 +1,6 @@
 import * as api from './api';
 import * as renderMarkup from './renderMarkup';
-import { list, form, warning, divError, filterForm, logo, paginationBar} from './refs';
+import { list, form, warning, divError, filterForm, logo, paginationBar, spinner} from './refs';
 import { loadLs, moviesDataUpdate, saveLs } from './storage';
 import { getSearchForm } from './filter';
 
@@ -46,6 +46,7 @@ if (refs.btnReset) {
 };
 
 function submitResetFilter(evn) {
+	spinner.classList.remove('done');
 	nextBtn.classList.remove('is-hidden')
   evn.preventDefault();
   refs.filterForm[0].options.selectedIndex = 0;
@@ -69,14 +70,17 @@ function submitResetFilter(evn) {
 		renderMarkup.renderMarkup(data);
 		moviesDataUpdate(data);
 		saveLs('total-pages', amountOfPages);
+		spinner.classList.add('done');
 	});
 	saveLs('page-pg', page);
 	clearPagination(amountOfPages)
 }
 
+
 logo.addEventListener('click', onLogoClick);
 
 function onLogoClick() {
+	spinner.classList.remove('done');
 	amountOfPages = 1000;
   saveLs('page-pg', 1);
   saveLs('genre-pg', '');
@@ -88,6 +92,7 @@ function onLogoClick() {
 		renderMarkup.renderMarkup(data);
 		moviesDataUpdate(data);
 		saveLs('total-pages', amountOfPages);
+		spinner.classList.add('done');
 	});
 }
 
@@ -108,6 +113,7 @@ let sort = loadLs('sort-pg');
 
 function eventGenre(evn) {
   if (evn) {
+		spinner.classList.remove('done');
     nextBtn.classList.remove('is-hidden');
     genre = evn.target.value;
     page = 1;
@@ -122,12 +128,14 @@ function eventGenre(evn) {
       }
       clearPagination(amountOfPages);
       saveLs('total-pages', amountOfPages);
+			spinner.classList.add('done');
     });
   }
 }
 
 function eventYear(evn) {
   if (evn) {
+		spinner.classList.remove('done');
     page = 1;
     saveLs('page-pg', page);
     year = evn.target.value;
@@ -140,6 +148,7 @@ function eventYear(evn) {
         amountOfPages = data.total_pages;
       }
       clearPagination(amountOfPages);
+			spinner.classList.add('done');
 			if(query=='') {
 				saveLs('total-pages', amountOfPages);
 			}
@@ -165,6 +174,7 @@ function eventYear(evn) {
 }
 function eventSort(evn) {
   if (evn) {
+		spinner.classList.remove('done');
     page = 1;
     saveLs('page-pg', page);
     sort = evn.target.value;
@@ -178,6 +188,7 @@ function eventSort(evn) {
       }
       clearPagination(amountOfPages);
       saveLs('total-pages', amountOfPages);
+			spinner.classList.add('done');
     });
   }
 }
@@ -190,10 +201,12 @@ if (nextBtn) {
 
 
 if(location.pathname.split("/").slice(-1) != 'library.html') {
+	spinner.classList.remove('done');
 	getSearchForm(page, query, genre, year, sort).then(data => {
 		renderMarkup.renderMarkup(data);
 		moviesDataUpdate(data);
 		saveLs('total-pages', amountOfPages);
+		spinner.classList.add('done');
 		if(query) {
 			document.querySelector('#genreForm').classList.add('is-hidden');
 			document.querySelector('#sortForm').classList.add('is-hidden');
@@ -307,6 +320,7 @@ function onPageClick(e) {
 }
 
 function onNextBtnClick() {
+	spinner.classList.remove('done');
   if (page == amountOfPages - 1) {
     nextBtn.classList.add('is-hidden');
   }
@@ -354,11 +368,13 @@ function onNextBtnClick() {
     });
     renderMarkup.renderMarkup(data);
     moviesDataUpdate(data);
+		spinner.classList.add('done');
   });
   saveLs('page-pg', page);
 }
 
 function onPrevBtnClick() {
+	spinner.classList.remove('done');
   if (page == amountOfPages) {
     nextBtn.classList.remove('is-hidden');
   }
@@ -412,11 +428,13 @@ function onPrevBtnClick() {
     });
     renderMarkup.renderMarkup(data);
     moviesDataUpdate(data);
+		spinner.classList.add('done');
   });
   saveLs('page-pg', page);
 }
 
 function renderPagination(e) {
+	spinner.classList.remove('done');
   page = parseInt(e.target.textContent);
   if (amountOfPages > 1 && amountOfPages < 6) {
     paginationBar.children[page - 1].classList.remove('active');
@@ -524,6 +542,7 @@ function renderPagination(e) {
     });
     renderMarkup.renderMarkup(data);
     moviesDataUpdate(data);
+		spinner.classList.add('done');
   });
   saveLs('page-pg', page);
 }
@@ -546,6 +565,7 @@ function clearPagination(amountOfPages) {
 }
 
 function search(e) {
+	spinner.classList.remove('done');
   e.preventDefault();
   genre = '';
 	document.querySelector('#genreForm').classList.add('is-hidden');
@@ -570,6 +590,7 @@ function search(e) {
     amountOfPages = data.total_pages;
     saveLs('total-pages', amountOfPages);
     clearPagination(amountOfPages);
+		spinner.classList.add('done');
     if (amountOfPages === 1) {
       prevBtn.classList.add('is-hidden');
       paginationBar.innerHTML = `<li class="page active">1</li>`;
