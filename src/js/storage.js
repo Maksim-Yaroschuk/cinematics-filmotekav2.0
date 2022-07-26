713edcfc3fd7b7a46dc379685a1d57cf8401f6f7
TkachAn <4ndrey.tkachenko@gmail.com>
2022-07-26 19:25:35 +0300

    Update storage.js

--- src/js/storage.js
+++ src/js/storage.js
@@ -24,16 +24,15 @@ const removeLs = key => {
   }
 };
 
-///localStorage.clear();
-const moviesData = loadLs('moviesData');
+// const moviesData = loadLs('moviesData');
 // console.log('moviesData', moviesData);
-const WatchedData = loadLs('WatchedData');
+// const WatchedData = loadLs('WatchedData');
 // console.log('WatchedData', WatchedData);
-const QueueData = loadLs('QueueData');
+// const QueueData = loadLs('QueueData');
 // console.log('QueueData', QueueData);
-const Watched = loadLs('Watched');
+// const Watched = loadLs('Watched');
 // console.log('Watched', Watched);
-const Queue = loadLs('Queue');
+// const Queue = loadLs('Queue');
 // console.log('Queue', Queue);
 
 function addListLibrary(id, select) {
@@ -53,10 +52,35 @@ function addListLibrary(id, select) {
   }
   saveLs(select, libArr);
   saveLs(sel, libData);
-
+  //moviesData.inclues;
 }
 
-export { addListLibrary, funAddQueue, saveLs, loadLs, removeLs };
+// async function addListLibrary(id, select) {
+// 	const sel = select + 'Data';
+// 	const moviesData = loadLs('moviesData');
+// 	const movieData = moviesData.find(movie => movie.id === id);
+// 	//const movieData = await getMovieDetails(id);
+// 	console.log('moviesData', moviesData);
+// 	console.log('movieData', movieData);
+// 	console.log('movData',movData);
+// 	const libArr = loadLs(select) ? loadLs(select) : [];
+// 	const libData = loadLs(sel) ? loadLs(sel) : [];
+//  	const index = libArr.indexOf(id);
+// 	if (index < 0) {
+// 		if (loadLs(sel) || loadLs(sel) !== 0) {
+// 			libArr.push(id);
+// 			libData.push(movData);
+// 		}
+//   } else {
+//     libArr.splice(index, 1);
+//     //const libData = loadLs(sel);
+//     libData.splice(index, 1);
+//   }
+//   saveLs(select, libArr);
+//   saveLs(sel, libData);
+
+// }
+export { addListLibrary, saveLs, loadLs, removeLs };
 export function moviesDataUpdate(data) {
   localStorage.setItem('moviesData', JSON.stringify(data.results));
 }
